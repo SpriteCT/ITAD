3f5063cd723670cfc8ee7812ae35d70cd54de7bb
SpriteCT <weqaetfs@gmail.com>
2025-03-23 15:45:18 +0100

    reapir nginx logs

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker stop $(docker ps -aq)
 docker container rm -f $(docker container ls -aq)
 docker volume rm -f $(docker volume ls -q)
 docker rm $(docker ps -a -q)
-docker rmi $(docker images -a -q)
+docker-compose up --build -d
